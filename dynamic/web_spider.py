21ea3dfe3fe9643d0053f39d00bf44610cfe438e
GJBLUE <895733091@qq.com>
2015-07-08 13:52:24 +0800

    可取到相应数据

--- dynamic/web_spider.py
+++ dynamic/web_spider.py
@@ -5,88 +5,81 @@ import urllib
 import urllib2
 import cookielib
 import httplib
+import pdb
+import json
+import re
+import sys
+reload(sys)
+sys.setdefaultencoding( "utf-8" )
 
 class ce:
-
-	#初始化
-	def __init__(self):
-		#POST发送地址
-		self.startUrl = 'http://www.17ce.com/site/http'
-		#代理IP
-		self.proxyURL = 'http://120.193.146.97:843'
-		#headers信息
-		self.headers = {
-			'User-Agent':'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.130 Safari/537.36',
-            'Referer':'www.17ce.com',
-            'Proxy-Connection':'keep-alive',
-            'Origin':'http://www.17ce.com',
-            'Content-type':'application/x-www-form-urlencoded',
-            'Content-length':'223',
-            'Host':'www.17ce.com',
-            'Accept-Encoding':'gzip, deflate, sdch',
-            'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
-            'Connection':'keep-alive',
-            'DNT':'1',
-            'Cookie':'maxid=35415; PHPSESSID=4vo49788584nukp9f0675jka77; __utmt=1; __utma=45994468.1573840565.1435998656.1435998656.1435998656.1; __utmb=45994468.1.10.1435998656; __utmc=45994468; __utmz=45994468.1435998656.1.1.utmcsr=baidu|utmccn=(organic)|utmcmd=organic; allSites=www.baidu.com'
+    #初始化
+    def __init__(self):
+	#POST发送地址
+	self.startUrl = 'http://www.17ce.com/site/ajaxfresh'
+	#代理IP
+	self.proxyURL = 'http://120.193.146.97:843'
+	#headers信息
+	self.headers = {
+            'User-Agent':'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.130 Safari/537.36',
+            'Referer':'http://www.17ce.com/'
+           # 'Origin':'http://www.17ce.com',
+           # 'Content-type':'application/x-www-form-urlencoded',
+           # 'Content-length':'223',
+           # 'Host':'www.17ce.com',
+           # 'Accept-Encoding':'gzip, deflate',
+           # 'Connection':'keep-alive',
+           # 'DNT':'1'
                 }
 		#POST数据
-		self.post = post = {
-			'url':'www.baidu.com',
-    		'curl':'',
-    		'rt':'1',
-    		'nocache':'0',
-    		'host':'',
-    		'referer':'',
-    		'cookie':'',
-    		'agent':'',
-    		'speed':'',
-    		'verify':'www.baidu.com18371013654',
-    		'pingcount':'',
-    		'pingsize':'',
-    		'area[]':'0',
-    		'area[]':'1',
-    		'area[]':'2',
-    		'area[]':'3',
+	self.post = urllib.urlencode({
+            'tid':'201507_efe33f28599811a97e8de18bf2913df1',
+            'num':'2',
+            'ajax_over':'0'
+		})
 
-    		'isp[]':'0',
-    		'isp[]':'1',
-    		'isp[]':'2',
-    		'isp[]':'6',
-    		'isp[]':'7',
-    		'isp[]':'8',
-    		'isp[]':'4'
-		}
-		#将POST数据进行编码转换
-		self.postData = urllib.urlencode(self.post)
-		#设置代理
-		self.proxy = urllib2.ProxyHandler({'http':self.proxyURL})
-		#设置cookie
-		self.cookie = cookielib.LWPCookieJar()
-		#设置cookie处理器
-		self.cookieHandler = urllib2.HTTPCookieProcessor(self.cookie)
-		#打开网页用到的opener，它的open == urllib2.urlopen
-		self.opener = urllib2.build_opener(self.cookieHandler, self.proxy, urllib2.HTTPHandler)
 
-	#main
-	def main(self):
+    #main
+    def main(self):
+        #将startUrl,postData,headers传入
+        request = urllib2.Request(self.startUrl,self.post,self.headers)
+        #发送请求
+        try:
+            #打开网页
+            response = urllib2.urlopen(request)
+         
+    	    page = response.read()
+            print type(page)
+            # a = eval(page)
+            # print a 
+            # pattern = re.compile(r'\\|<.*?>', re.I|re.M|re.S)
+            # filedata = pattern.sub('', page)
+            # with open('c://Code/json.txt', 'w') as f:
+            #     f.write(filedata)
+                
+            # with open('c://Code/json.txt', 'r') as f:
+            #     # for line in f:
+            #     #     print line
+            #     d = f.read()
+            #print type(d)
+    	    # datas = json.dumps(page, ensure_ascii=False)
+
+         #    jsondatas = json.loads(datas).encode('utf-8')
+            data = eval(page)
+            datas = json.dumps(data,ensure_ascii=False)
+            print type(datas)
+            jsondatas = json.loads(datas)
+            #print jsondatas['freshdata']['2965']['name']
+            data1 = jsondatas['freshdata']
+    	    print type(data1)
+
+                #mydict = eval(jsondatas)
+                
+
+    	except httplib.HTTPException, e:
+    	    pass 
             
-	    #将startUrl,postData,headers传入
-            request = urllib2.Request(self.startUrl,self.postData,self.headers)
-	    #发送请求
-	    #response = self.opener.open(request)
-            print '@'*10
-            try:
-                #打开网页
-            	response = urllib2.urlopen(request)
-                print type(response)
-            	#获取内容
-    	    	page = response.read()
-                print 123
-    	    	print '#'*10+page
-            except httplib.HTTPException, e:
-	        pass 
-	    
-	   
+
 
 s = ce()
 s.main()
