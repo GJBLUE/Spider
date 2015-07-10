343bf5a0a4d9776b5d26b18f634bc7488c07dadd
GJBLUE <895733091@qq.com>
2015-07-10 15:33:49 +0800

    urllib2与requests两版本

---
+++ dynamic/web_spider.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+import urllib
+import urllib2
+import re
+import json
+import pdb
+import httplib
+import requests
+from requests import Request, Session
+import sys
+
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+testUrl = 'www.bilibili.com'
+
+
+
+headers = {
+            'User-Agent':'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.130 Safari/537.36',
+            'Referer':'http://www.17ce.com/',
+            'Origin':'http://www.17ce.com',
+            'Content-type':'application/x-www-form-urlencoded',
+ 
+            'Host':'www.17ce.com',
+            #'Accept-Encoding':'gzip, deflate',
+
+            'Accept':'*/*',
+            'DNT':'1'
+        }
+
+
+#获取POST后网页返回的数据
+def getPage(url, post, headers):
+    #获取response
+    response = requests.post(url, data = post, headers = headers, stream=False)
+    
+    page = response.content
+    # s = Session()
+    # req = Request('POST', url, data = post, headers = headers)
+    # prepped = req.prepare()
+    # resp = s.send(prepped)
+    # page = resp.content
+    
+    data = eval(page, {}, {})
+    datas = json.dumps(data,ensure_ascii=False)
+    jsondatas = json.loads(datas)
+    return jsondatas
+    
+
+
+
+#发送第一次POST所获得数据
+def getFirstData():
+    #POST地址
+    targetUrl = 'http://www.17ce.com/site/http'
+    #post = urllib.urlencode(post1)
+    #print post
+    post = '&url=%s&curl=&rt=1&nocache=0&host=&referer=&cookie=&agent=&speed=&verify=%s18371013654&pingcount=&pingsize=&area[]=0&area[]=1&area[]=2&area[]=3&&isp[]=0&isp[]=1&isp[]=2&isp[]=6&isp[]=7&isp[]=8&isp[]=4&'%(testUrl,testUrl)
+    #print post
+    #pdb.set_trace()
+    #以dict形式获取到所需的数据
+    data = getPage(targetUrl, post, headers)
+    #print data
+    #print data['tid']
+    return data['tid']
+
+def getSecondData():
+    #此处获得的是对应输入网址的post表单信息，获得对于该网址的关键
+    tids = getFirstData()
+    #第二个POST表单
+    post2 = {
+        'tid':tids,
+
+        'num':'1',
+
+        'ajax_over':'0'
+    }
+    post = urllib.urlencode(post2)
+    #第二个POST网址
+    targetUrl = 'http://www.17ce.com/site/ajaxfresh'
+    #以dict形式获取到所需的数据
+    datas = getPage(targetUrl, post, headers)
+    #print datas
+    data = datas['freshdata']
+    #print type(data)
+    #print data
+    for key , value in data.items():
+        print value['name'], value['speed']
+
+
+
+if __name__ == '__main__':
+    getSecondData()
