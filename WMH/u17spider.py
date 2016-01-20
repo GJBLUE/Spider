29e8f32fa9df5257096d5ff47b6670153bcf92e3
GJBLUE <895733091@qq.com>
2016-01-20 16:29:40 +0800

    update weibo

--- WMH/u17spider.py
+++ WMH/u17spider.py
@@ -42,7 +42,7 @@ def analyze_manga(targeturl):
 def analyze_chapter():
     #此处输入你想要爬取的微漫画地址
     itemss = analyze_manga('http://www.u17.com/comic/3166.html')
-    items = itemss[80:81]
+    items = itemss[6:11]
     print len(items)
     for i in xrange(len(items)):
         #items[i][0]:url, items[i][1]:title
@@ -51,30 +51,35 @@ def analyze_chapter():
         p = response.content
         titles = items[i][1].replace('！', '')
         titles = titles.replace(' ', '')
-        print titles
+        titles = delextra(titles)
         #获取所有的漫画url
         images = re.findall('"src":"(.*?)"', p)
         images = [unicode(base64.b64decode(i)) for i in images]
         for url in images:
             #对漫画url进行处理，获取到真正的漫画链接
             pages.append(url)
+        #print titles, type(titles)
         mkdir(titles)
-        print len(pages)
+        print len(pages), titles
         for y in xrange(len(pages)):
             saveImg(pages[y], titles, y)
-            time.sleep(1)
+            time.sleep(0.5)
 
 
 def delextra(picnum):
-    pattern = re.compile(r'\:|\/', re.I|re.M|re.S)
+    pattern = re.compile(r'\:|\\', re.I|re.M|re.S)
     pic = pattern.sub('', picnum)
     return pic  
 
 #传入图片地址，文件名，保存单张图片
 def saveImg(imageURL,name, number):
-    fileNames = "d:/zhj" + "/" +name + "/" + str(number) + ".jpg"
-    u = urllib.urlopen(imageURL)
-    data = u.read()
+    num = '%03d'%number
+    print num
+    fileNames = "d:/queue" + "/" +name + "/" + str(num) + ".jpg"
+    #u = urllib.urlopen(imageURL)
+    #data = u.read()
+    u = requests.get(imageURL)
+    data = u.content
     f = open(fileNames, 'wb')
     f.write(data)
     f.close()
@@ -82,7 +87,7 @@ def saveImg(imageURL,name, number):
 #创建新目录
 def mkdir(path):
     path = path.strip()
-    path = os.path.join('d:/zhj',path)
+    path = os.path.join('d:/queue',path)
     isExists=os.path.exists(path)
     # 判断结果
     if not isExists:
