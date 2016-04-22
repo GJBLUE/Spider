80ba2a9c87aab7c9a5021c0e5c3f083e1fde2130
GJBLUE <895733091@qq.com>
2016-04-22 13:50:14 +0800

    Filtlering data using the pipeline

--- Managa/Managa/pipelines.py
+++ Managa/Managa/pipelines.py
@@ -6,6 +6,37 @@
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
 
-class ManagaPipeline(object):
-    def process_item(self, item, spider):
-        return item
+
+
+
+class u17Pipeline(object):
+    
+    def process_item(self, item, u17Spider):
+        all_managa = []
+        single_managa = dict()
+        
+        print item
+#        for data in item:
+#            print data['cvip']
+#            vip_mark = data['cvip']
+#            if len(vip_mark) > 1:
+#                pass
+#            elif len(vip_mark) == 1:
+#                single_managa.setdefault('cname', data['cname'][0])
+#                single_managa.setdefault('curl', data['curl'][0])
+#            else:
+#                single_managa.setdefault('cname', data['cname'][-1])
+#                single_managa.setdefault('curl', data['curl'][-1])
+#            all_managa.append(single_managa)
+
+        vip_mark = item['cvip']
+        if len(vip_mark) > 1:
+            pass
+        elif len(vip_mark) == 1:
+            single_managa.setdefault('cname', item['cname'][0])
+            single_managa.setdefault('curl', item['curl'][0])
+        else:
+            single_managa.setdefault('cname', item['cname'][-1])
+            single_managa.setdefault('curl', item['curl'][-1])
+        #all_managa.append(single_managa)
+        return single_managa
