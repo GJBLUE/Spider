5a9e83894fa1c18c5031e3dafc1806618686b6c5
GJBLUE <895733091@qq.com>
2016-04-22 11:05:02 +0800

    get new free managa chapter

---
+++ Managa/Managa/spiders/detect.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+from scrapy.spider import Spider
+from scrapy.selector import Selector
+from Managa.items import ManagaItem
+
+class u17Spider(Spider):
+
+    name = "u17"
+    allowed_domains = ["u17.com"]
+
+    start_urls = [
+        "http://www.u17.com/comic/190.html",
+        "http://www.u17.com/comic/13707.html"
+    ]
+
+    def parse(self, response):
+        sel = Selector(response)
+        sites = sel.xpath('//*[@id="chapter"]/li')
+        items = []
+
+#        for site in sites:
+#            item = ManagaItem()
+#            item['cname'] = site.xpath('a/@title').extract()[-2:]
+#            item['curl'] = site.xpath('a/@href').extract()[-2:]
+#            item['cvip'] = site.xpath('a/@class').extract()[-2:]
+#            items.append(item)
+#        return items
+        
+        item = ManagaItem()
+        item['cname'] = sites.xpath('a/@title').extract()[-2:]
+        item['curl'] = sites.xpath('a/@href').extract()[-2:]
+        item['cvip'] = sites.xpath('a/@class').extract()[-2:]
+        return item
+
